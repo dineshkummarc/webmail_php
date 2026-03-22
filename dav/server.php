4f7edc7f7f46c5aa0dfa3e5e87c7a72f96602913
Dinesh Kumar <mapmydestination@gmail.com>
2026-03-22 22:05:08 +0530

    updated

--- dav/server.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-defined('WM_ROOTPATH') || define('WM_ROOTPATH', (dirname(__FILE__).'/../'));
-
-require_once WM_ROOTPATH.'libraries/afterlogic/DAV/autoload.php';
-
-$server = new afterlogic_DAV_Server();
-$server->exec();
--- libraries/afterlogic/DAV/Share/Backend/Abstract.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-abstract class afterlogic_DAV_Share_Backend_Abstract {
-	
-	abstract function UpdateShare($sCalendarId, $FromUser, $ToUser, $Mode);
-}
